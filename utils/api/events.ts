73fe7c05aedd8c9a5418759bcc477120f2fbcb74
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-11-05 14:05:46 +0200

    refactor: Added types but this is not recommended
    I used a bad method of typing coz axios didnt want to allow my routes it was using T instead of that i gave it
    
    so i decided to statically type the reponses

--- utils/api/events.ts
+++ utils/api/events.ts
@@ -1,25 +1,25 @@
 import API from "@/utils/axios";
-import {EventCreateData} from "@/lib/types";
+import {EventRequest, EventResponse, EventsResponse} from "@/utils/types";
 
-export const getAllEvents = async (page=1, limit=12) => {
-    const {data} = await API.get(`/events/?page=${page}&limit=${limit}`);
-    return data
+export const getAllEvents = async (page=1, limit=12): Promise<EventsResponse> => {
+    const {data, status} = await API.get<{data: EventsResponse}>(`/events/?page=${page}&limit=${limit}`) ;
+    return data as EventsResponse;
 }
 
-export const createEvent = async (eventData: EventCreateData) => {
+export const createEvent = async (eventData: EventRequest): Promise<EventResponse> => {
     const { data } = await API.post("/events", eventData);
-    return data;
+    return data as EventResponse;
 };
 
 
-export const getEventBySlug = async (slug: string) => {
+export const getEventBySlug = async (slug: string): Promise<EventResponse> => {
     const { data } = await API.get(`/events/${slug}`);
-    return data;
+    return data as EventResponse;
 };
 
-export const updateEventBySlug = async (slug: string, eventData: object) => {
+export const updateEventBySlug = async (slug: string, eventData: object): Promise<EventResponse> => {
     const { data } = await API.put(`/events/${slug}`, eventData);
-    return data;
+    return data as EventResponse;
 };
 
 export const deleteEventBySlug = async (slug: string) => {
